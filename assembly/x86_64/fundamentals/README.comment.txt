5bcce0f5a4127c95420d3abf479bef9f86187377
Brian Salehi <salehibrian@gmail.com>
2020-12-18 21:45:46 +0330

    x86_64 practices created

---
+++ assembly/x86_64/fundamentals/README.comment.txt
@@ -0,0 +1,2 @@
+This chapter holds fundamentals of x86_64 Assembly and the least topics you
+need to know about it before you proceed the main practices.
---
+++ assembly/x86_64/fundamentals/structure.asm
@@ -0,0 +1,21 @@
+section .data
+	message db 'hello, world', 10, 0
+	length equ $ - message - 1
+
+section .bss
+	response resb 20
+
+section .text
+	global main
+
+main:
+	mov rax, 1			; write system call
+	mov rdi, 1			; standard output
+	mov rsi, message	; message address in memory
+	mov rdx, length		; length of message
+	syscall
+
+	mov rax, 60			; exit system call
+	mov rdi, 0			; return 0
+	syscall
+
