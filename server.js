1d9b025c947af54651062fd77bfa8a169f066ab7
Brian Salehi <salehibrian@gmail.com>
2020-12-27 02:58:37 +0330

    links broken intentionally, server modified

--- server.js
+++ server.js
@@ -24,23 +24,32 @@ app.get("/*.html", (req, res) => {
 });
 
 app.get("/*", (req, res) => {
-	fs.stat(`html-files/${req.path}`, function (err, stat) {
+	console.log(`html-files${req.path}`)
+	fs.stat(`html-files${req.path}`, function (err, stat) {
 		if (err == null) {
-			fs.readlink(`html-files/${req.path}`, function(lerr, filepath) {
-				if (err == null) {
-					res.status(200).sendFile(path.join(__dirname, filepath));
-				} else {
-					res.status(404).sendFile(path.join(__dirname, 'no-practice.html'));
-				}
+			if (stat.isFile()) {
+				console.log("file")
+				fs.readlink(`html-files${req.path}`, function(lerr, filepath) {
+					if (lerr == null) {
+						res.status(200).sendFile(path.join(__dirname, filepath.toString()));
+					} else {
+						res.status(404).sendFile(path.join(__dirname, 'no-practice.html'));
+					}
+				});
+			} else if (stat.isDirectory()) {
+				console.log("dir")
+				fs.readlink(`html-files${req.path}`, function(lerr, filepath) {
+					if (lerr == null) {
+						res.status(200).sendFile(path.join(__dirname, filepath.toString()));
+					} else {
+						res.status(404).sendFile(path.join(__dirname, 'no-practice.html'));
+					}
+				});
+			} else {
+				res.status(404).sendFile(path.join(__dirname, 'no-practice.html'));
 			}
 		} else {
-			fs.stat(`html-files/${req.path}/index`, function (derr, dstat) {
-				if (derr == null) {
-					res.status(200).sendFile(path.join(__dirname, `html-files/${req.path}/index`));
-				} else {
-					res.status(404).sendFile(path.join(__dirname, 'no-practice.html'));
-				}
-			}
+			res.status(404).sendFile(path.join(__dirname, 'no-practice.html'));
 		}
 	});
 });
