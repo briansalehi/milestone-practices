c7cc8ed1dd47df342fbf9297c7c7f6f8b9f10dc4
Brian Salehi <salehibrian@gmail.com>
2021-01-03 19:36:32 +0330

    aliases added

---
+++ linux/text-processing/awk.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cat << EOF > /tmp/sample
+a1,a2,a3
+b1,b2,b3
+c1,c2,c3
+EOF
+
+awk 'BEGIN{ FS = "," } if NR == 2 { print $2 }' /tmp/sample
---
+++ linux/text-processing/tac.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "first line" > /tmp/sample
+echo "second line" >> /tmp/sample
+echo "third line" >> /tmp/sample
+
+tac /tmp/sample
