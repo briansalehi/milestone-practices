9e7add41a99c5c5975173f52825ebe4ca56a2581
Brian Salehi <salehibrian@gmail.com>
2021-01-06 12:32:47 +0330

    cad tools section created

---
+++ electronics/cad/README.comment.txt
@@ -0,0 +1,6 @@
+<b>CAD</b> softwares are tools which electronic and computer engineers use to create
+electronic devices in high precision.
+Of these samples, <i>Printed Circut Boards</i> is one product which CAD
+softwares are capable of designing.
+
+In this section, CAD tools are reviewed.
---
+++ linux/package-management/apt.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# updates apt repository
+apt update
+
+# lists upgradable packages
+apt list --upgradable
+
+# upgrades installed packages
+apt upgrade
+
+# searches through all packages
+apt search gedit
+
+# prints package information
+apt show gedit
+
+# installs packages
+apt install gedit
+
+# remove packages
+apt remove gedit
+
+# removes packages with dependencies
+apt purge gedit
+
+# removes unnecessary packages
+apt autoremove
+
+# installs missing packages
+apt install --fix-missing
+
+# installs missing dependencies
+apt install --fix-broken
