953d6331727ed50576d644d8777249b7b676f187
Brian Salehi <salehibrian@gmail.com>
2021-01-22 23:04:03 +0330

    configuration changed

--- cpp/gui/widgets/table-view.cxx
+++ cpp/gui/widgets/table-view.cxx
@@ -19,21 +19,17 @@ int main(int argc, char **argv)
 	QTableView *table = new QTableView;
 	QStandardItemModel *model = new QStandardItemModel(1, 3, window);
 
-	table->setContextMenuPolicy(Qt::CustomContextMenu);
-	table->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
 	model->setHorizontalHeaderItem(0, new QStandardItem(QString("Name")));
 	model->setHorizontalHeaderItem(1, new QStandardItem(QString("Date of Birth")));
 	model->setHorizontalHeaderItem(2, new QStandardItem(QString("Phone Number")));
+
+	model->setItem(0, 0, new QStandardItem(QString("Jane Doe")));
+	model->setItem(0, 1, new QStandardItem(QDate(1990, 1, 1).toString()));
+	model->setItem(0, 2, new QStandardItem(QString("123")));
+
+	table->setContextMenuPolicy(Qt::CustomContextMenu);
+	table->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
 	table->setModel(model);
-
-	QStandardItem *firstItem = new QStandardItem(QString("Jane Doe"));
-	QDate birthdate(1990, 1, 1);
-	QStandardItem *secondItem = new QStandardItem(birthdate.toString());
-	QStandardItem *thirdItem = new QStandardItem(QString("0123456789"));
-
-	model->setItem(0, 0, firstItem);
-	model->setItem(0, 1, secondItem);
-	model->setItem(0, 2, thirdItem);
 
 	layout->addWidget(table);
 	window->setLayout(layout);
