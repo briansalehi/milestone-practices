bb02c7cd836f9cff1323ef8c128db2f185017a92
Brian Salehi <salehibrian@gmail.com>
2021-02-09 22:45:37 +0330

    function chapter complete

--- cpp/fundamentals/functions/reference.cxx
+++ cpp/fundamentals/functions/reference.cxx
@@ -1,21 +1,13 @@
-#include <iostream>
-
 const double pi = 3.14159;
 
-// don't return area, change the value of variable itself in the argument!
-void Area(double radius, double &area) {
-    area = pi*radius*radius;
+void Area(double radius, double& area)
+{
+    area = pi * radius * radius;
 }
 
 int main()
 {
     double radius = 2, area = 0;
-    std::cout << "enter radius: ";
-    std::cin >> radius;
-    std::cout << radius << std::endl;
-
     Area(radius, area);
-    std::cout << area << std::endl;
-
     return 0;
 }
