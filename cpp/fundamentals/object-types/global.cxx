bbca0bb3b23e62896c0d530c9640b55620252fb2
Brian Salehi <salehibrian@gmail.com>
2021-02-23 14:51:21 +0330

    cxx global variable update

--- cpp/fundamentals/object-types/global.cxx
+++ cpp/fundamentals/object-types/global.cxx
@@ -4,12 +4,7 @@ int global = 10;
 int main()
 {
     int local = 20;
-
+	// homonym local scope variables hide global variables
+	int global = 30;
     return 0;
 }
-
-/*
-    use global variables only if you do know exactly what you are doing
-    generally using global variables in large applications is considered
-    poor design.
-*/
