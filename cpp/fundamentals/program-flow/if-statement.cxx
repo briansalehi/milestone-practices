36b14e1f007ce28a5c88cf1f1d8c1f8a4831f071
Brian Salehi <salehibrian@gmail.com>
2021-04-12 22:01:38 +0430

    rename cpp condition control page

---
+++ cpp/fundamentals/program-flow/if-statement.cxx
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	// since stdc++17
+    if (bool condition = true ; condition) {
+		std::cout << "Condition evaluated true" << std::endl;
+	} else {
+		std::cout << "Condition evaluated false" << std::endl;
+	}
+
+    return 0;
+}
