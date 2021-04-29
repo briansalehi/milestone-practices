ee3d0d765f4a08eaf13bbfe351228ce04a4892a1
Brian Salehi <salehibrian@gmail.com>
2021-04-29 16:16:00 +0430

    general update

--- assembly/x86_64/memory-sections.asm
+++ assembly/x86_64/memory-sections.asm
@@ -9,17 +9,11 @@ section .text
 	global main
 
 main:
-	push rbx
-	mov rbx, rsi
-
 	mov rax, 1			; write system call
 	mov rdi, 1			; standard output
 	mov rsi, message	; message address in memory
 	mov rdx, length		; length of message
 	syscall
-
-	mov rsi, rbx
-	pop rbx
 
 	mov rax, 60			; exit system call
 	mov rdi, 0			; return 0
