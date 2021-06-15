857593f747756565940687200910ebcfb208bc04
Brian Salehi <salehibrian@gmail.com>
2021-06-15 19:57:57 +0430

    qt: modify fundamentals/widget source

--- cpp/gui/fundamentals/widget.cxx
+++ cpp/gui/fundamentals/widget.cxx
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
 {
 	QApplication app(argc, argv);
 
-	QWidget *window = new QWidget;
+	auto *window = new QWidget;
 	window->setWindowTitle("Widget as Window");
 	window->resize(250, 150);
 	window->show();
